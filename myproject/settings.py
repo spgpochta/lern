87cc4081c46199de1c312034f9cfdd143818c1b3
spgpochta <spgpochta@gmail.com>
2018-07-25 20:58:37 +0300

    Изменил пути статики.

--- myproject/settings.py
+++ myproject/settings.py
@@ -122,6 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/home/oleg/YandexDisk/Python_Lessons/Django/myproject/myproject/static/'
+STATIC_URL = '/static/admin/'
+STATIC_ROOT = '/home/oleg/YandexDisk/Python_Lessons/Django/myproject/static/admin/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "ststic"),)
