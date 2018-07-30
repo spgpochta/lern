e0cdc1515366f6bf2f1f0fc1a225486272be1075
spgpochta <spgpochta@gmail.com>
2018-07-30 17:26:32 +0300

    перенёс в аутхапп фьюху, админ и пр из дз8

---
+++ authapp/tests.py
@@ -0,0 +1,10 @@
+# from django.test import TestCase
+# from .models import ShopUser
+# Create your tests here.
+
+
+# class TestShopUser(TestCase):
+#     def SetUp(self):
+#         user = ShopUser() # = models.ImageField(
+# upload_to= 'users_avatars' , blank=True)
+#         age = models.PositiveIntegerField(verbose_name='возраст')
