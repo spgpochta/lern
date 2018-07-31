27a7aa0b6f051ff8e61230c5e4d47ef9be8477e7
spgpochta <spgpochta@gmail.com>
2018-07-31 17:38:43 +0300

    аучапп урл. добавил едит и регистер.

--- authapp/urls.py
+++ authapp/urls.py
@@ -6,6 +6,8 @@ app_name = 'authapp'
 urlpatterns = [
 
     path('login/', authapp.login, name='login'),
-    path('logout/', authapp.logout, name='logout')
+    path('logout/', authapp.logout, name='logout'),
+    path('register/', authapp.register, name='register'),
+    path('edit/', authapp.edit, name='edit'),
 
 ]
