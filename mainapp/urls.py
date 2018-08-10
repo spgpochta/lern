992e775e76625c2bc5714cebb5fb3a4797276675
spgpochta <spgpochta@gmail.com>
2018-08-10 21:13:34 +0300

    Починил пагинатор. Надо чтобы урл формировал правильный адрес в адресной строке. Типа такого /products/category/3/page/1/. Переход на продукт не работает.

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,7 +7,7 @@ app_name = 'mainapp'
 
 urlpatterns = [
 
-    path('product/<int:pk>/', mainapp.products, name='product'),
+    path('product/<int:pk>/', mainapp.product, name='product'),
     path('category/<int:pk>/', mainapp.products, name='category'),
     url(r'^category/(?P<pk>\d+)/page/(?P<page>\d+)/$', mainapp.products,
         name='page')
