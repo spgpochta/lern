8dc927c3bf090e1a6f70c11630a661c9bc399f3f
spgpochta <41076626+spgpochta@users.noreply.github.com>
2018-08-11 10:43:29 +0300

    Update urls.py
    Добавил проброс статики для гуникорн

--- myproject/urls.py
+++ myproject/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include
 import adminapp as adminapp
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
@@ -35,3 +36,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+
+    urlpatterns += staticfiles_urlpatterns()
