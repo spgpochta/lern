6f89d25dfb88532a3d03735fe18467470af55154
spgpochta <41076626+spgpochta@users.noreply.github.com>
2018-08-11 11:29:28 +0300

    Update settings.py
    Скобку не закрыл.

--- myproject/settings.py
+++ myproject/settings.py
@@ -126,10 +126,11 @@ DATE_FORMAT = 'd E Y'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"))
                     # '/home/oleg/git/myproject/static/admin/')
 
 MEDIA_URL = '/media/'
+                    
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'authapp.ShopUser'
