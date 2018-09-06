eca98d89c807dc2b245e3803c8bb45da82a72db1
spgpochta <spgpochta@gmail.com>
2018-09-06 13:04:03 +0300

    "Купить" включилось. Не передаёт цену, исправить! На записи в БД падает, исправить!

--- adminapp/views.py
+++ adminapp/views.py
@@ -35,6 +35,22 @@ def orders(request):
 
     return render(request, 'adminapp/orders.html', context)
 
+def OrderCreate(request):
+    cart = Basket(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+            cart.clear()
+            return render(request, 'orders/order/created.html', {'order': order})
+
+    form = OrderCreateForm()
+    return render(request, 'orders/order/create.html', {'cart': cart,
+                                                        'form': form})
 
 class CaptionMixin(ContextMixin):
 
